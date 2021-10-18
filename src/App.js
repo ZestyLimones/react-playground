378831a748e7463d744cb866d58d9da65ebcc2b1
Micheal Limones <micheal@zestylimones.com>
2021-10-18 14:08:04 -0600

    playing around with composition.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Split from './composition/Split';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Split className="left" flexBasis={2}>
+        This is the content for the left `Split`. Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Incidunt ex velit suscipit facere
+        officia?
+      </Split>
+      <Split className="right">
+        This is the content for the right `Split`. Inventore aliquid cupiditate
+        suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus
+        id soluta aspernatur.
+      </Split>
+    </main>
   );
 }
 
